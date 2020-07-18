246a78d59af88722f8302fdca737e4878c892a85
dmitryro <dmitryro@gmail.com>
2020-07-18 19:34:49 -0400

    send

--- broker/agents.py
+++ broker/agents.py
@@ -8,13 +8,12 @@ logger = logging.getLogger(__name__)
 @app.agent(ship_topic)
 async def process_answers(questions):
     answers = []
+    anser_text = "our answer to "
     async for question in questions:
-        answer_text = "I DID MY BEST AND I ANSWERED {} asked by {}"
-        answer_text = answer_text.format(question.text, question.slack_id)
         answer = Answer(id=1, question_id=question.id, 
                         slack_id=question.slack_id, 
                         question=question.text, 
-                        answer=answer_text)
+                        answer=answer_text+f" - {question.slack_id} {question.text}")
         answers.append(answer)
         logger.info(answer_text)
     ship_topic.send(value=answers)
