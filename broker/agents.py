988c15195955de31b945ee7e2a0f26aeabf11d81
dmitryro <dmitryro@gmail.com>
2020-07-18 19:28:00 -0400

    send

--- broker/agents.py
+++ broker/agents.py
@@ -1,6 +1,5 @@
-import faust
 import logging
-from broker.models import Question, Answer
+from broker.models import Answer
 from broker.app import app, ship_topic
 
 logger = logging.getLogger(__name__)
@@ -8,10 +7,10 @@ logger = logging.getLogger(__name__)
 
 @app.agent(ship_topic)
 async def process_answers(questions):
-    answeres = []
-    
+    answers = []
     async for question in questions:
-        answer_text = f"I DID MY BEST AND I ANSWERED {question.text} by {question.slack_id}"     
+        answer_text = "I DID MY BEST AND I ANSWERED {} asked by {}"
+        answer_text = answer_text.format(question.text, question.slack_id)
         answer = Answer(id=1, question_id=question.id, 
                         slack_id=question.slack_id, 
                         question=question.text, 
