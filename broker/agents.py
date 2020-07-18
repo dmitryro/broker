8811a1a2a29cba993e6e277b174df6a11ba9a592
dmitryro <dmitryro@gmail.com>
2020-07-18 18:37:25 -0400

    add model Answer

--- broker/agents.py
+++ broker/agents.py
@@ -1,6 +1,6 @@
 import faust
 import logging
-from broker.models import Question
+from broker.models import Question, Answer
 from broker.app import app, ship_topic
 
 logger = logging.getLogger(__name__)
@@ -11,5 +11,12 @@ async def process_answers(questions):
     answeres = []
 
     async for question in questions:
-        answers.append("ANSWERED {question.text}")
+    answer_text = f"I DID MY BEST AND I ANSWERED {question.text} by {question.slack_id}"     
+        answer = Answer(id=1, question_id=question.id, 
+                        slack_id=question.slack_id, 
+                        question=question.text, 
+                        answer=answer_text)
+        answers.append(answer)
+
+    logger.info(f"I DID MY BEST AND I ANSWERED {question.text} by {question.slack_id}")
     ship_topic.send(value=answers)
--- broker/models.py
+++ broker/models.py
@@ -1,6 +1,18 @@
 import faust
 
 class Question(faust.Record):
+    id: int
     slack_id: str
     channel_id: str
     text: str
+
+
+class Answer(faust.Record):
+    id: int
+    question_id: int
+    channel_id: str
+    question: str
+    answer: str
+
+
+
