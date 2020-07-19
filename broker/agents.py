4a15ad6d88f5419743ff12eeff14f7a49c4e380c
dmitryro <dmitryro@gmail.com>
2020-07-18 21:59:28 -0400

    update agent

--- broker/agents.py
+++ broker/agents.py
@@ -3,18 +3,17 @@ from broker.models import Answer
 from broker.app import app, ship_topic
 
 logger = logging.getLogger(__name__)
-
+answers_table = app.Table('answers_table', default=Answer)
 
 @app.agent(ship_topic)
 async def process_answers(questions):
     answers = []
     anser_text = "our answer to "
-    async for question in questions:
+    async for question in questions.group_by(Questionw.id):
         answer = Answer(id=1, question_id=question.id, 
                         slack_id=question.slack_id, 
                         question=question.text, 
                         answer=answer_text+f" - {question.slack_id} {question.text}")
-        answers.append(answer)
-        logger.info(answer_text)
-    ship_topic.send(value=answers)
-    yield answers
+        answers_table[question.id]=answer
+        logger.info(f"Question received. Question Id {question.id}")
+        yield question
